700108d3ba9badd7c4a68e49f788a8c960de1b2a
Nguyễn Tấn Thoại <thoaint@bap.jp>
2023-06-30 16:16:57 +0700

    set webhook

--- src/utils/bot.ts
+++ src/utils/bot.ts
@@ -4,6 +4,7 @@ let bot: TelegramBot | undefined;
 
 if (!bot) {
   bot = new TelegramBot(process.env.NEXT_PUBLIC_TOKEN as string, { polling: true });
+  bot.setWebHook('https://cal-volume-trade-chat-bot.vercel.app/api/webhook');
 }
 
 export default bot;
