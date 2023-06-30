4883dc6b057c5a166835edb679dbc11905e04701
Nguyễn Tấn Thoại <thoaint@bap.jp>
2023-06-30 16:40:53 +0700

    update

--- src/utils/bot.ts
+++ src/utils/bot.ts
@@ -1,6 +1,9 @@
 const sendMessage = (chatId: string, message: string) => {
   return fetch(`https://api.telegram.org/bot${process.env.NEXT_PUBLIC_TOKEN}/sendMessage`, {
     method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
     body: JSON.stringify({
       chat_id: chatId,
       text: message,
