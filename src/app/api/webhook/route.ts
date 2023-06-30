b9b1500fc4f7cdb7dc1ed04a8124a9cdff680300
Nguyễn Tấn Thoại <thoaint@bap.jp>
2023-06-30 17:06:21 +0700

    update

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -8,18 +8,18 @@ export async function POST(request: Request) {
     const text = data.message.text?.trim();
     const textE = text.split(',');
     if (textE.length < 3)
-      return await bot?.sendMessage(chatId, 'Vui lòng nhập đúng định dạng. "profit,tp,sl"');
-    // PROFIT,TP,SL
-    const profit = isNaN(Number(textE[0])) ? 0 : Number(textE[0]);
-    const tp = isNaN(Number(textE[1])) ? 0 : Number(textE[1]);
+      return await bot?.sendMessage(chatId, 'Vui lòng nhập đúng định dạng. "lost,entry,sl"');
+    // LOST,ENTRY,SL
+    const lost = isNaN(Number(textE[0])) ? 0 : Number(textE[0]);
+    const entry = isNaN(Number(textE[1])) ? 0 : Number(textE[1]);
     const sl = isNaN(Number(textE[2])) ? 0 : Number(textE[2]);
-    if (tp === sl)
-      return await bot?.sendMessage(chatId, 'TP và SL không nên bằng nhau. Vui lòng nhập lại.');
-    const delta = Math.abs(tp - sl);
-    const volume = Number(profit / delta).toFixed(4);
+    if (entry === sl)
+      return await bot?.sendMessage(chatId, 'Entry và SL không nên bằng nhau. Vui lòng nhập lại.');
+    const delta = Math.abs(entry - sl);
+    const volume = Number(lost / delta).toFixed(4);
     await bot?.sendMessage(
       chatId,
-      `Volume của giao dịch PROFIT $${profit} - TP $${tp} - SL $${sl} là ${volume}`,
+      `Volume của giao dịch có số chịu lỗ $${lost} - Entry $${entry} - SL $${sl} là ${volume}`,
     );
     return new Response('Done!');
   } catch (error) {
